694b606788c7e7125c2743f6a9e6c3e6895af304
Mohamed Emad <mohamed.emad45621@gmail.com>
2025-03-22 21:46:36 +0200

    navigate to the home screen if user wasa signed already

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,10 @@
 import 'package:chatbot/core/routing/app_router.dart';
+import 'package:chatbot/core/services/firebase_auth_service.dart';
 import 'package:chatbot/core/utils/app_images.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
+
+import '../../../../../core/services/service_locator.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -35,7 +38,11 @@ class _SplashViewBodyState extends State<SplashViewBody> {
 
   void executeNavigation() {
     Future.delayed(const Duration(seconds: 2), () {
-      GoRouter.of(context).pushReplacement(AppRouter.kSignInView);
+      if (getIt.get<FirebaseAuthService>().isLoggedIn()) {
+        GoRouter.of(context).pushReplacement(AppRouter.kHomeView);
+      } else {
+        GoRouter.of(context).pushReplacement(AppRouter.kSignInView);
+      }
     });
   }
 }
